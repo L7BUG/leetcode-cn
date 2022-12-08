dede66a5a2bd161778a44420a806a4505f12e83f
luliangyu <luliangyu@seewintech.com>
2022-12-08 17:40:58 +0800

    21

---
+++ src/test/java/com/l/Demo141.java
@@ -0,0 +1,10 @@
+package com.l;
+
+/**
+ * Demo141
+ *
+ * @author luliangyu
+ * @date 2022-12-08 14:02
+ */
+public class Demo141 {
+}
---
+++ src/test/java/com/l/Demo21.java
@@ -0,0 +1,90 @@
+package com.l;
+
+import lombok.ToString;
+import org.junit.Test;
+
+/**
+ * 合并两个有序链表
+ * <a href='https://leetcode.cn/problems/merge-two-sorted-lists/'>21. 合并两个有序链表</a>
+ *
+ * @author luliangyu
+ * @date 2022-12-08 14:35
+ */
+public class Demo21 {
+
+	@Test
+	public void test() {
+		ListNode list1 = new ListNode(1, new ListNode(3, new ListNode(8, new ListNode(9))));
+		ListNode list2 = new ListNode(1, new ListNode(4, new ListNode(5)));
+
+
+		long millis = System.currentTimeMillis();
+		System.err.println(mergeTwoLists1(list1, list2));
+		System.err.println("耗时" + (System.currentTimeMillis() - millis));
+
+
+		millis = System.currentTimeMillis();
+		System.err.println(mergeTwoLists2(list1, list2));
+		System.err.println("耗时" + (System.currentTimeMillis() - millis));
+	}
+
+	public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
+		if (list1 == null || list2 == null) {
+			if (list1 != null) {
+				return list1;
+			} else {
+				return list2;
+			}
+		}
+		int val;
+		if (list1.val < list2.val) {
+			val = list1.val;
+			return new ListNode(val, mergeTwoLists1(list1.next, list2));
+		} else {
+			val = list2.val;
+			return new ListNode(val, mergeTwoLists1(list1, list2.next));
+		}
+	}
+
+	public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+		ListNode head = new ListNode(-1);
+		ListNode p = head;
+		for (; list1 != null || list2 != null; p = p.next) {
+			if (list1 == null) {
+				p.next = list2;
+				break;
+			}
+			if (list2 == null) {
+				p.next = list1;
+				break;
+			}
+			p.next = new ListNode();
+			if (list1.val < list2.val) {
+				p.next.val = list1.val;
+				list1 = list1.next;
+			} else {
+				p.next.val = list2.val;
+				list2 = list2.next;
+			}
+		}
+		return head.next;
+	}
+
+	@ToString
+	public static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+}
