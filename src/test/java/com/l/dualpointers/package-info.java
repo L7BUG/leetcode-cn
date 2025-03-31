565aac69796dfef30475e9a18e88f3390428760f
L7-BUG <ll789y@gmail.com>
2025-03-31 10:28:08 +0800

    package-info.java

--- src/test/java/com/l/dualpointers/package-info.java
+++ src/test/java/com/l/dualpointers/package-info.java
@@ -1,5 +1,4 @@
 /**
- * 双指针相关
- * https://labuladong.online/algo/essential-technique/array-two-pointers-summary-2/
+ * <a href="https://labuladong.online/algo/essential-technique/array-two-pointers-summary-2/">双指针相关</a>
  */
 package com.l.dualpointers;
