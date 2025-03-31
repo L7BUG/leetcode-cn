2283b08d72d9fa493b4a5f0d4591fe7e3f765939
L7-BUG <ll789y@gmail.com>
2025-03-31 10:24:52 +0800

    [344]-反转字符串

---
+++ src/test/java/com/l/dualpointers/Demo344.java
@@ -0,0 +1,21 @@
+package com.l.dualpointers;
+
+/**
+ * <a href='https://leetcode.cn/problems/reverse-string/description/'>[344]-反转字符串</a>
+ *
+ * @author Administrator
+ * @since 2025/3/31 10:21
+ */
+public class Demo344 {
+
+	public void reverseString(char[] s) {
+		int slow = 0, fast = s.length - 1;
+		while (slow < fast) {
+			char temp = s[slow];
+			s[slow] = s[fast];
+			s[fast] = temp;
+			slow++;
+			fast--;
+		}
+	}
+}
