7dd237bec8f75e1f299a8b0934007939b766e26b
L7-BUG <ll789y@gmail.com>
2025-07-02 10:21:13 +0800

    [3]-无重复字符的最长子串

--- src/test/java/com/l/sliding/window/Demo03Test.java
+++ src/test/java/com/l/sliding/window/Demo03Test.java
@@ -47,7 +47,11 @@ public class Demo03Test {
 	public void valid() {
 		for (String item : data) {
 			System.err.printf("item = [%s]%n", item);
-			Assertions.assertEquals(lengthOfLongestSubstring(item), lengthOfLongestSubstring2(item));
+			int a = lengthOfLongestSubstring(item);
+			int b = lengthOfLongestSubstring2(item);
+			int c = lengthOfLongestSubstring3(item);
+			Assertions.assertEquals(a, b);
+			Assertions.assertEquals(c, b);
 		}
 	}
 
@@ -95,4 +99,30 @@ public class Demo03Test {
 		}
 		return max;
 	}
+
+	public int lengthOfLongestSubstring3(String s) {
+		// 哈希集合，记录每个字符是否出现过
+		Set<Character> occ = new HashSet<>(s.length());
+		int n = s.length();
+		// 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+		int rk = -1, ans = 0;
+		for (int i = 0; i < n; ++i) {
+			if (i != 0) {
+				// 左指针向右移动一格，移除一个字符
+				occ.remove(s.charAt(i - 1));
+			}
+			while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+				// 不断地移动右指针
+				occ.add(s.charAt(rk + 1));
+				++rk;
+			}
+			// 第 i 到 rk 个字符是一个极长的无重复字符子串
+			ans = Math.max(ans, rk - i + 1);
+		}
+		return ans;
+	}
+	// 作者：力扣官方题解
+	// 链接：https://leetcode.cn/problems/longest-substring-without-repeating-characters/solutions/227999/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetc-2/
+	// 来源：力扣（LeetCode）
+	// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 }
